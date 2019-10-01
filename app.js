5cbc544083d523aa176d1ea6cad2b70946691ba6
vikesh <vicky.v@vedaha.com>
2019-10-01 18:26:43 +0530

    2nd mixo

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => res.send('Hello World! this is vicky '))
+app.get('/', (req, res) => res.send('Hello World! this is mixo '))
 
 
 
-app.listen(port, () => console.log(`Example app listening on port vikesh ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port mixo ${port}!`))
